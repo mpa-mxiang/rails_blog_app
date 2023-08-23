78142cb2bf931f2a24458b69a275576e09923265
Marc raphael <marcraphael979@gmail.com>
2023-08-24 00:47:01 +0100

    db/schema: Update Post and Comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_23_135152) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_23_214244) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_23_135152) do
     t.bigint "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "Text"
     t.index ["author_id"], name: "index_comments_on_author_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
   end
@@ -45,6 +46,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_23_135152) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "author_id", null: false
+    t.string "Title"
     t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
