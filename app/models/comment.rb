56bb8504ba7e668ae307ef35aacfe7ac631b168f
Marc raphael <marcraphael979@gmail.com>
2023-08-24 01:07:55 +0100

    app/models/comment: Write a method that updates the comments counter for a post

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,17 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  # defining callbacks in the `Comment` model.
+  after_create :update_comments_counter
+  after_destroy :update_comments_counter
+
+  private
+
+  def update_comments_counter
+    # updating the `CommentsCounter` attribute
+    # of the associated `post` object with the count of comments that belong to that post. It is using
+    # the `update` method provided by ActiveRecord to update the attribute value.
+    post.update(CommentsCounter: post.comments.count)
+  end
 end
