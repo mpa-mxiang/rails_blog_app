521c64a3d1144428d2be34f263ed8dce1b58cf6a
Marc raphael <marcraphael979@gmail.com>
2023-08-24 01:15:28 +0100

    app/models/user: Fix linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,19 @@
 class User < ApplicationRecord
-# Define a one-to-many association. In this case, it
-# is defining the associations for the `User` model.
+  # Define a one-to-many association. In this case, it
+  # is defining the associations for the `User` model.
   has_many :posts, foreign_key: :author_id
   has_many :comments, foreign_key: :author_id
   has_many :likes
 
   def recent_posts(limit = 3)
-    #retrieve the most recent posts for a user.
-		posts.order(created_at: :desc).limit(limit)
+    # retrieve the most recent posts for a user.
+    posts.order(created_at: :desc).limit(limit)
   end
 
   def update_posts_counter
-    #updating the `posts_counter` attribute of the
-		# `User` model with the count of the user's posts. This is useful for keeping track of the number
-		# of posts a user has.
-		update(posts_counter: posts.count)
+    # updating the `posts_counter` attribute of the
+    # `User` model with the count of the user's posts. This is useful for keeping track of the number
+    # of posts a user has.
+    update(posts_counter: posts.count)
   end
 end
