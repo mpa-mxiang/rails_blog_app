5915233553b9f715d4edf97d9daa5f8b70a35615
Marc raphael <marcraphael979@gmail.com>
2023-08-25 09:58:49 +0100

    Add validations

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  it 'comments_counter of post should update' do
+    author = User.new(Name: 'Tom', Photo: 'https://unsplash.com/photos/F_-0BxGuVvo', Bio: 'Teacher from Mexico.')
+    post = Post.new(author:, Title: 'Hello', Text: 'This is my first post')
+    comment = Comment.new(author:, post:)
+    author.save
+    post.save
+    comment.save
+    expect(post.comments_counter).to eq(1)
+  end
+end
