0c5d7057fd395c5fbfec5f18637a8318bd5bfd79
Marc raphael <marcraphael979@gmail.com>
2023-08-25 11:01:11 +0100

    spec/models: Update unit tests

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe Comment, type: :model do
   it 'comments_counter of post should update' do
     author = User.new(Name: 'Tom', Photo: 'https://unsplash.com/photos/F_-0BxGuVvo', Bio: 'Teacher from Mexico.')
-    post = Post.new(author:, Title: 'Hello', Text: 'This is my first post')
-    comment = Comment.new(author:, post:)
+    post = Post.new(author_id:, Title: 'Hello', Text: 'This is my first post')
+    comment = Comment.new(author_id:, post_id:)
     author.save
     post.save
     comment.save
--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 RSpec.describe Like, type: :model do
   it 'likes_counter of post should update' do
     author = User.new(Name: 'Tom', Photo: 'https://unsplash.com/photos/F_-0BxGuVvo', Bio: 'Teacher from Mexico.')
-    post = Post.new(author_id:, Title: 'Hello', Text: 'This is my first post')
-    like = Like.new(author_id:, post_id:)
+    post = Post.new(author_id: author.id, Title: 'Hello', Text: 'This is my first post')
+    like = Like.new(author_id: author.id, post_id:)
     author.save
     post.save
     like.save
-    expect(post.likes_counter).to eq(1)
+    expect(post.LikesCounter).to eq(1)
   end
 end
