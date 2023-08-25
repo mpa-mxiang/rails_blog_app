f3cb630ab00dcb2916de9bae3ba14ac7c668e0b2
Marc raphael <marcraphael979@gmail.com>
2023-08-25 12:08:16 +0100

    app/models/user: Add docs

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,10 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: :author_id
   has_many :likes, foreign_key: :author_id
 
+  # validating that the `Name` attribute of the `User`
+  # model is present, meaning it cannot be blank or nil.
   validates :Name, presence: true
+
   validates :PostCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
 
   def recent_posts(limit = 3)
