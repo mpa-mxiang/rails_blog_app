fdfd0d90ac301b1ce2c9e9b57d7690d023ce670c
Marc raphael <marcraphael979@gmail.com>
2023-08-26 02:07:07 +0100

    spec/models/like_spec: Add default values

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Like, type: :model do
   it 'likes_counter of post should update' do
-    author = User.new(Name: 'Tom', Photo: 'https://unsplash.com/photos/F_-0BxGuVvo', Bio: 'Teacher from Mexico.')
-    post = Post.new(author:, Title: 'Hello', Text: 'This is my first post')
+    author = User.new(Name: 'Tom', Photo: 'https://unsplash.com/photos/F_-0BxGuVvo', Bio: 'Teacher.', PostCounter: 0)
+    post = Post.new(author:, Title: 'Hello', Text: 'This is my first post', CommentsCounter: 0, LikesCounter: 0)
     like = Like.new(author:, post:)
     author.save
     post.save
