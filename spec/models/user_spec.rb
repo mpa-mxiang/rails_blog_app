fc6f06b588c11a0c71f10b56874bd0f5557fd848
Marc raphael <marcraphael979@gmail.com>
2023-08-26 02:09:49 +0100

    spec/models/user_spec:  Apply changes required

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject { User.new(Name: 'Tom', Photo: 'https://unsplash.com/photos/F_-0BxGuVvo', Bio: 'Teacher from Mexico.') }
+  subject { User.new(Name: 'Tom', Photo: 'https://unsplash.com/photos/F_-0BxGuVvo', Bio: 'Teacher from Mexico.', PostCounter: 0) }
 
   before { subject.save }
   it 'name should be present' do
@@ -19,11 +19,25 @@ RSpec.describe User, type: :model do
     expect(subject).to be_valid
   end
 
+  it 'Postcounter is not a numerical value' do
+    subject.PostCounter = 'not_numeric'
+    subject.save
+    expect(subject).to_not be_an_instance_of(Numeric)
+  end
+
+  it 'posts_counter of author should update' do
+    post = Post.new(author: subject, Title: 'Hello', Text: 'first post', LikesCounter: 0, CommentsCounter: 0)
+    post.save
+    subject.save
+    expect(subject.PostCounter).to eq(1)
+  end
+
   it 'recent posts should return the last 3' do
-    first_post = Post.new(author: subject, Title: 'Hello', Text: 'This is my first post')
-    second_post = Post.new(author: subject, Title: 'Hello', Text: 'This is my second post')
-    third_post = Post.new(author: subject, Title: 'Hello', Text: 'This is my third post')
-    fourth_post = Post.new(author: subject, Title: 'Hello', Text: 'This is my fourth post')
+    first_post = Post.new(author: subject, Title: 'Hello', Text: 'first post', LikesCounter: 0, CommentsCounter: 0)
+    second_post = Post.new(author: subject, Title: 'Hello', Text: 'second post', LikesCounter: 0, CommentsCounter: 0)
+    third_post = Post.new(author: subject, Title: 'Hello', Text: 'tirth post', LikesCounter: 0, CommentsCounter: 0)
+    fourth_post = Post.new(author: subject, Title: 'Hello', Text: 'fourth post', LikesCounter: 0, CommentsCounter: 0)
+    subject.save
     first_post.save
     second_post.save
     third_post.save
