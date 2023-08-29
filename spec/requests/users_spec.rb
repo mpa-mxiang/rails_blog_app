587571f2db45527e501d765a028ec695c0cd3d3d
Marc raphael <marcraphael979@gmail.com>
2023-08-29 20:35:17 +0100

    spec/requests/users_spec: Add test for a success request

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,1 +1,30 @@
+require 'rails_helper'
 
+RSpec.describe 'Users', type: :request do
+  let(:user) do
+    User.create(
+      Name: 'Marc',
+      Photo: 'http://www.image.com',
+      Bio: 'Developer',
+      PostsCounter: 5
+    )
+  end
+
+  before do
+    get '/users'
+  end
+
+  describe 'User GET/ index ' do
+    it 'returns user http success' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders user template' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'user responsed body with correct place holder' do
+      expect(response.body).to include('All Users')
+    end
+  end
+end
