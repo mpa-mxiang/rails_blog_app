d5d3b4abbd698d05533beb7466c429a68f70c5ae
Marc raphael <marcraphael979@gmail.com>
2023-08-29 21:45:25 +0100

    spec/requests/posts_spec: Add test for index action

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -14,9 +14,23 @@ RSpec.describe 'Posts', type: :request do
     Post.create(
       Title: 'Ruby',
       Text: 'Learn ruby',
-      Author_id: user,
+      author_id: user,
       CommentsCounter: 5,
       LikesCounter: 10
     )
+  end
+
+	describe 'GET /index' do
+    before do
+      get "/users/#{user.id}/posts/"
+    end
+
+    it 'renders post template' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'post response body includes correct placeholder text' do
+      expect(response.body).to include('List of all posts')
+    end
   end
 end
