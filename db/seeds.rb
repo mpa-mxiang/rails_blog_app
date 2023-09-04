811343bb0c0ce09c17e59cc7868b5eb5615dd3cc
benja27 <50673336+benja27@users.noreply.github.com>
2023-09-04 11:56:55 -0600

    update database config

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+# Este archivo seed crea varios usuarios de ejemplo en la base de datos.
+
+# Define el número de usuarios que deseas crear
+num_users = 10
+puts 'creando usuarios'
+# Utiliza un bucle times para crear usuarios con datos aleatorios
+num_users.times do |i|
+  User.create(
+    name: "User #{i + 1}",
+    photo: "Photo #{i + 1}",
+    bio: "Bio #{i + 1}",
+    postCounter: 0    
+  )
+  puts 'ususario creado exitosamente'
+end
+
+# Puedes ajustar el número de usuarios y los datos aleatorios según tus necesidades.
+
+
+
